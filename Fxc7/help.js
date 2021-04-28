8146d76ff950183577d39d0057d10a4247aaad16
anandanandu <83335461+anandanandu@users.noreply.github.com>
2021-04-28 19:35:52 +0530

    Add files via upload

---
+++ Fxc7/help.js
@@ -0,0 +1,465 @@
+// menu fitur bot
+const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal) => { 
+	return `
+	
+╭──────「 *REGULATION ${name}* 」
+┴
+┣⊱ *NAMA USER* : *${pushname2}*
+┣⊱ *VERIVICATION:*  ✅
+┣⊱ *LIMIT* : *${limitt} perhari*
+┣⊱ *AKTIF* : ${kyun(uptime)}
+┣⊱ *JAM* : *${jam} WIB*
+┣⊱ *TANGGAL* : *${tanggal}*
+┣⊱ *VERSION* : *6.5.0*
+┣⊱ *USER TERDAFTAR* : *${user.length} User*
+┣⊱  ❌ *SPAM*
+┣⊱  ❌ *CALL & VC*
+┣⊱ *Melanggar*?? *Banned + Out Group*
+┣⊱ *Youtube* : *${yt}*
+┣⊱ *Instagram* : *${instagram}*
+┬
+╰────────────────────────
+
+
+╭──────「 *ABOUT ${name}* 」
+┴
+│➻ *${prefix}report bug 👾*
+│➻ *${prefix}info*
+│➻ *${prefix}donation*
+│➻ *${prefix}owner*
+│➻ *${prefix}speed*
+│➻ *${prefix}daftar*
+│➻ *${prefix}limit*
+│➻ *${prefix}totaluser*
+│➻ *${prefix}blocklist*
+│➻ *${prefix}banlist*
+│➻ *${prefix}premiumlist*
+│➻ *${prefix}language*
+┬
+╰────────────────────────
+
+
+͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭──────「 *MEDIA DOWNLOADER* 」
+┴
+│➻ *${prefix}tiktokstalk username*
+│➻ *${prefix}igstalk doc_.editor_mmx*
+│➻ *${prefix}instagram link*
+│➻ *${prefix}instavid link valid*
+│➻ *${prefix}instaimg link valid*
+│➻ *${prefix}instastory username*
+│➻ *${prefix}ssweb url*
+│➻ *${prefix}url2img Url*
+│➻ *${prefix}tiktok*
+│➻ *${prefix}phototiktok*
+│➻ *${prefix}kbbi*
+│➻ *${prefix}wait*
+│➻ *${prefix}trendtwit*
+│➻ *${prefix}google type u want*
+┬
+╰────────────────────────
+
+
+╭──────「 *CREATOR MENU* 」
+┴
+│➻ *${prefix}quotemaker tx/wtrmk/tema*
+│➻ *${prefix}nulis text*
+├───────────────────
+│➻ *${prefix}tp 1 - 162*
+│➻ *${prefix}ep 1 - 216*
+│➻ *${prefix}tahta Name*
+│➻ *${prefix}coffe Name*
+│➻ *${prefix}love Name*
+│➻ *${prefix}glow Name*
+│➻ *${prefix}bpink Name*
+│➻ *${prefix}3d Name*
+│➻ *${prefix}tlight Name*
+│➻ *${prefix}lighttext Name*
+│➻ *${prefix}crismes Name*
+│➻ *${prefix}naruto Name*
+│➻ *${prefix}goldbutton Name*
+│➻ *${prefix}silverbutton Name*
+│➻ *${prefix}flame Name*
+│➻ *${prefix}leavest Name*
+│➻ *${prefix}pubg Name/name2*
+│➻ *${prefix}phlogo Name/name2*
+│➻ *${prefix}glitch Name/name2/b*
+│➻ *${prefix}retro Name/name2/b*
+│➻ *${prefix}tts id Haii*
+│➻ *${prefix}ttp Name*
+│➻ *${prefix}cpaper Name*
+├───────────────────
+│➻ *${prefix}stiker*
+│➻ *${prefix}gifstiker*
+│➻ *${prefix}toimg*
+│➻ *${prefix}img2url*
+│➻ *${prefix}tomp3*
+│➻ *${prefix}ocr*
+┬
+╰──────────────────────────
+
+
+╭───────「 *GROUP ONLY* 」
+┴
+│➻ *${prefix}modeanime On/Off*
+│➻ *${prefix}neonime naruto*
+│➻ *${prefix}naruto*
+│➻ *${prefix}minato*
+│➻ *${prefix}boruto*
+│➻ *${prefix}hinata*
+│➻ *${prefix}sakura*
+│➻ *${prefix}sasuke*
+│➻ *${prefix}toukachan*
+│➻ *${prefix}rize*
+│➻ *${prefix}akira*
+│➻ *${prefix}itori*
+│➻ *${prefix}kurumi*
+│➻ *${prefix}miku*
+│➻ *${prefix}anime*
+│➻ *${prefix}animecry*
+│➻ *${prefix}animekiss*
+┬
+╰───────────────────────
+
+
+╭───────「 *GROUP ONLY* 」
+┴
+│➻ *${prefix}antilink On/Off*
+│➻ *${prefix}welcome On/Off*
+│➻ *${prefix}grup buka/tutup*
+│➻ *${prefix}ownergrup*
+│➻ *${prefix}setpp*
+│➻ *${prefix}infogc*
+│➻ *${prefix}add +91xxxxxxxxxx*
+│➻ *${prefix}kick @mentioned*
+│➻ *${prefix}kicktime @mentioned*
+│➻ *${prefix}promote @mentioned*
+│➻ *${prefix}demote @mentioned*
+│➻ *${prefix}setname*
+│➻ *${prefix}setdesc*
+│➻ *${prefix}linkgrup*
+│➻ *${prefix}tagme*
+│➻ *${prefix}hidetag*
+│➻ *${prefix}tagall*
+│➻ *${prefix}mentionall*
+│➻ *${prefix}fitnah @mentioned/isi/balasan*
+│➻ *${prefix}listadmin*
+┬
+╰────────────────────────
+
+
+╭───────「 *GROUP ONLY ADMIN* 」
+┴
+│➻ *${prefix}nsfw On/Off*
+│➻ *${prefix}nsfwloli*
+│➻ *${prefix}nsfwblowjob*
+│➻ *${prefix}nsfwneko*
+│➻ *${prefix}nsfwtrap*
+│➻ *${prefix}hentai*
+│➻ *${prefix}simih On/Off*
+│➻ *${prefix}bott on/off*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}anjing*
+│➻ *${prefix}kucing*
+│➻ *${prefix}testime*
+│➻ *${prefix}hilih*
+│➻ *${prefix}apakah*
+│➻ *${prefix}kapankah*
+│➻ *${prefix}bisakah*
+│➻ *${prefix}cantik*
+│➻ *${prefix}ganteng*
+│➻ *${prefix}rate*
+│➻ *${prefix}watak*
+│➻ *${prefix}hobby*
+│➻ *${prefix}infogempa*
+│➻ *${prefix}infonomor*
+│➻ *${prefix}quotes*
+│➻ *${prefix}truth*
+│➻ *${prefix}dare*
+│➻ *${prefix}katabijak*
+│➻ *${prefix}fakta*
+│➻ *${prefix}darkjokes*
+│➻ *${prefix}bucin*
+│➻ *${prefix}pantun*
+│➻ *${prefix}katacinta*
+│➻ *${prefix}jadwaltvnow*
+│➻ *${prefix}hekerbucin*
+│➻ *${prefix}katailham*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}jarak Banyuwangi/Surabaya*
+│➻ *${prefix}translate en/How Areu?*
+│➻ *${prefix}pasangan Anjay/Awowkok*
+│➻ *${prefix}gantengcek Anjay*
+│➻ *${prefix}cantikcek Aowkwok*
+│➻ *${prefix}artinama Anjay*
+│➻ *${prefix}persengay Topan*
+│➻ *${prefix}pbucin Anjay*
+│➻ *${prefix}bpfont Anjay*
+│➻ *${prefix}textstyle Anjay*
+│➻ *${prefix}jadwaltv antv*
+│➻ *${prefix}lirik melukis senja*
+│➻ *${prefix}chord Melukis senja*
+│➻ *${prefix}wiki Adolf Hitler*
+│➻ *${prefix}brainly pertanyaan*
+│➻ *${prefix}resepmasakan rawon*
+│➻ *${prefix}map Banyuwangi*
+│➻ *${prefix}film Fast and Farious*
+│➻ *${prefix}pinterest gambar kucing*
+│➻ *${prefix}infocuaca India*
+│➻ *${prefix}map India*
+│➻ *${prefix}mimpi Ular*
+│➻ *${prefix}infoalamat jalan India*
+│➻ *${prefix}playstore WhatsApp*
+┬
+╰───────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}readmore*
+│➻ *${prefix}puisiimg*
+│➻ *${prefix}asupan*
+│➻ *${prefix}fml*
+│➻ *${prefix}tebakgambar*
+│➻ *${prefix}caklontong*
+│➻ *${prefix}family100*
+│➻ *${prefix}memeindo*
+│➻ *${prefix}kalkulator 13*12*
+│➻ *${prefix}moddroid lightroom*
+│➻ *${prefix}apkpure lightroom*
+│➻ *${prefix}searchfilm Doraemon*
+│➻ *${prefix}happymod lightroom*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}randomkpop*
+│➻ *${prefix}cersex*
+│➻ *${prefix}randombokep*
+│➻ *${prefix}pornhub stepMoms*
+│➻ *${prefix}xvideos japan*
+│➻ *${prefix}nekopoi oni chichi*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}jadwalsholat Inadia*
+│➻ *${prefix}quran*
+│➻ *${prefix}quranlist*
+│➻ *${prefix}quransurah 1*
+┬
+╰────────────────────────
+
+
+╭──────「 *FIND ME SIR* 」
+┴
+│➻ *${prefix}becrypt string*
+│➻ *${prefix}encode64 string*
+│➻ *${prefix}decode64 encrypt*
+│➻ *${prefix}encode32 string*
+│➻ *${prefix}decode32 encrypt*
+│➻ *${prefix}encbinary string*
+│➻ *${prefix}decbinary encrypt*
+│➻ *${prefix}encoctal string*
+│➻ *${prefix}decoctal encrypt*
+│➻ *${prefix}hashidentifier Encrypt Hash*
+│➻ *${prefix}dorking dork*
+│➻ *${prefix}pastebin teks*
+│➻ *${prefix}tinyurl link*
+│➻ *${prefix}bitly link*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}spamcall +91xxxxxxxxx*
+│➻ *${prefix}spamgmail contoh@gmail.com*
+┬
+╰────────────────────────
+
+
+╭──────「 *SOUND* 」
+┴
+│➻ *${prefix}iri*
+│➻ *${prefix}tapi*
+│➻ *${prefix}bernyanyi*
+│➻ *${prefix}anjay*
+┬
+╰────────────────────────
+
+
+╭─────────「 *OWNER ONLY* 」
+┴
+│➻ *${prefix}addprem mentioned*
+│➻ *${prefix}removeprem mention*
+│➻ *${prefix}setmemlimit*
+│➻ *${prefix}setlimit*
+│➻ *${prefix}setreply*
+│➻ *${prefix}setprefix*
+│➻ *${prefix}setnamebot*
+│➻ *${prefix}setppbot*
+│➻ *${prefix}bc*
+│➻ *${prefix}bcgc*
+│➻ *${prefix}ban*
+│➻ *${prefix}unban*
+│➻ *${prefix}block*
+│➻ *${prefix}unblock*
+│➻ *${prefix}clearall*
+│➻ *${prefix}delete*
+│➻ *${prefix}clone*
+│➻ *${prefix}getses*
+│➻ *${prefix}leave*
+┬
+╰────────────────────────
+
+
+╭────────「 *PREMIUM ONLY* 」
+┴
+│➻ *${prefix}play menepi*
+│➻ *${prefix}fb link video*
+│➻ *${prefix}snack link snack video*
+│➻ *${prefix}ytmp3 link yt*
+│➻ *${prefix}ytmp4 link yt*
+│➻ *${prefix}randomquran*
+│➻ *${prefix}joox Monolog Pamungkas*
+│➻ *${prefix}smule Link Video Smule*
+┬
+╰────────────────────────
+
+
+╭─────「 *SUPPORT ${name}* 」
+┴
+│➲ *PINKY*
+│➲ *DOC_.EDITOR_MMX*
+│➲ *INDONASIAN FRND*
+│➲ *ALEXAMANSOUR MUSIC*
+│➲ *MY TEAM FXC7 BOT*
+│➲ *RECOMENTED BY ALL*
+│➲ *SPONSER BY MALLU WALKERS*
+│➲ *LOL~4NK3R-EROR*
+│➲ *CONTENT CREATOR BOT WHATSAPP*
+┬
+╰────────────────────────`
+}
+
+exports.help = help
+
+// penghitung aktif bot
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
+
+// donasi menu
+const donasi = (name) => { 
+	return `       
+╭─────「 *DONASI SEIKHLASNYA* 」
+┴
+│√ *PHONE PAY : +91 82810 25658*
+│√ *GPAY : +91 82810 25658*
+│
+│\`\`\`Gpay\`\`\`
+│ *15k Invite Bot Ke Grup selama 1 bulan*
+│
+│\`\`\`PhonePay\`\`\`
+│ *20k Invite Bot 1 Lakh*
+┬
+╰──────「 *BY ${name}* 」
+
+ For the Survival of Dark Mallu  Bot,:'
+`
+}
+exports.donastion = donation
+
+// bahasa list
+const languag = (prefix) => {
+return `
+List Bahasa Untuk Command *${prefix}tts*
+
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.languag = language
+
+// Limit
+const limitend = (pushname2) => {
+        return`*Maaf ${pushname2} Limit Anda Hari Ini Sudah Habis\n\n Silahkan Kembali Lagi Hari Esok:)\n\n Limit Di Reset Ketik Owner Gabut:v*`
+}
+
+const limitcount = (limitCounts) => {
+        return`
+Limit Kamu: ${limitCounts}
+`
+}
+
+exports.limitend = limitend
+exports.limitcount = limitcount
